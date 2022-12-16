971f12d93ba4f6d97c483e363e186cebfce47f8c
Greg Rogers <greg.r.rogers@gmail.com>
2022-12-15 21:46:34 -0500

    Day 15 2022 Part 1

---
+++ 2022/day15/src/main.rs
@@ -0,0 +1,114 @@
+use std::collections::HashSet;
+use std::io::{self, Read};
+
+use regex::Regex;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+impl Pos {
+    fn new(x: i32, y: i32) -> Pos {
+        Pos { x, y }
+    }
+}
+
+struct Measurement {
+    sensor: Pos,
+    beacon: Pos,
+}
+
+
+fn parse(puzzle_input: &str) -> Vec<Measurement> {
+    let re = Regex::new("^Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)$").unwrap();
+    puzzle_input.trim_end().lines().map(|line| {
+        let cap = re.captures(line).unwrap();
+        let sensor_x = cap[1].parse().unwrap();
+        let sensor_y = cap[2].parse().unwrap();
+        let sensor = Pos::new(sensor_x, sensor_y);
+        let beacon_x = cap[3].parse().unwrap();
+        let beacon_y = cap[4].parse().unwrap();
+        let beacon = Pos::new(beacon_x, beacon_y);
+        Measurement { sensor, beacon }
+    }).collect()
+}
+
+fn manhattan_dist(pos1: Pos, pos2: Pos) -> i32 {
+    (pos1.x - pos2.x).abs() + (pos1.y - pos2.y).abs()
+}
+
+fn count_impossible_beacons(measurements: &[Measurement], row: i32) -> usize {
+    // TODO - Could definitely use an interval set to speed this up, but it's good enough
+    let mut xs = HashSet::new();
+    for m in measurements {
+        let sensor_dist = manhattan_dist(m.sensor, m.beacon);
+        let row_dist = (row - m.sensor.y).abs();
+        let dist_diff = sensor_dist - row_dist;
+        if dist_diff >= 0 {
+            for x in (m.sensor.x - dist_diff)..=(m.sensor.x + dist_diff) {
+                xs.insert(x);
+            }
+        }
+    }
+    for m in measurements {
+        if m.beacon.y == row {
+            xs.remove(&m.beacon.x);
+        }
+    }
+    xs.len()
+}
+
+fn find_distress_beacon(measurements: &[Measurement], max: i32) -> usize {
+
+    0 // FIXME
+}
+
+fn part1(measurements: &[Measurement]) -> usize {
+    count_impossible_beacons(measurements, 2000000)
+}
+
+fn part2(measurements: &[Measurement]) -> usize {
+    find_distress_beacon(measurements, 4000000)
+}
+
+fn main() {
+    let mut puzzle_input = String::new();
+    io::stdin().read_to_string(&mut puzzle_input).unwrap();
+
+    let measurements = parse(&puzzle_input);
+    println!("{}", part1(&measurements));
+    println!("{}", part2(&measurements));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EX: &str = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(count_impossible_beacons(&parse(EX), 10), 26);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(find_distress_beacon(&parse(EX), 20), 56000011);
+    }
+}
