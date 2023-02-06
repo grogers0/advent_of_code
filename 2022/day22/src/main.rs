51e08ef1c4fd61a80ec1c89f861d5e14e34dd76e
Greg Rogers <greg.r.rogers@gmail.com>
2023-02-05 21:16:32 -0500

    Day 22 2022 Part 2

--- 2022/day22/src/main.rs
+++ 2022/day22/src/main.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
 use std::io::{self, Read};
-use std::ops::Index;
+use std::ops::{Add, AddAssign};
 
 #[derive(Copy, Clone)]
 enum Turn {
@@ -11,7 +12,7 @@ enum Step {
     Turn(Turn),
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 enum Dir {
     Up, Down, Left, Right
 }
@@ -34,7 +35,7 @@ impl Dir {
         }
     }
 
-    fn score(self) -> usize {
+    fn score(self) -> isize {
         match self {
             Dir::Right => 0,
             Dir::Down  => 1,
@@ -42,112 +43,103 @@ impl Dir {
             Dir::Up    => 3,
         }
     }
+
+    fn as_offset(self) -> Pos {
+        match self {
+            Dir::Up    => Pos { x:  0,  y: -1 },
+            Dir::Down  => Pos { x:  0,  y:  1 },
+            Dir::Left  => Pos { x: -1,  y:  0 },
+            Dir::Right => Pos { x:  1,  y:  0 },
+        }
+    }
+
+    fn flip(self) -> Dir {
+        match self {
+            Dir::Up    => Dir::Down,
+            Dir::Down  => Dir::Up,
+            Dir::Left  => Dir::Right,
+            Dir::Right => Dir::Left,
+        }
+    }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
 struct Pos {
-    x: usize,
-    y: usize,
+    x: isize,
+    y: isize,
+}
+
+impl Add<Pos> for Pos {
+    type Output = Pos;
+    fn add(self, other: Pos) -> Pos {
+        let mut pos = self;
+        pos += other;
+        pos
+    }
+}
+
+impl AddAssign<Pos> for Pos {
+    fn add_assign(&mut self, other: Pos) {
+        self.x += other.x;
+        self.y += other.y;
+    }
 }
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 enum Tile {
-    Open, Wall, StepThrough
+    Open, Wall
 }
 
 
 
 struct Grid {
-    width: usize,
-    height: usize,
-    tiles: Vec<Tile>,
+    initial_pos: Pos,
+    tiles: HashMap<Pos, Tile>,
+    block_size: usize,
+    jumps: HashMap<(Pos, Dir), (Pos, Dir)>,
 }
 
 impl Grid {
-    fn walk_one(&self, pos: Pos, dir: Dir) -> Pos {
-        let init_pos = pos;
-        match dir {
-            Dir::Up => {
-                let x = pos.x;
-                let ys1 = (0..pos.y).rev();
-                let ys2 = (pos.y..self.height).rev();
-                for y in ys1.chain(ys2) {
-                    match self[Pos{x, y}] {
-                        Tile::Open => return Pos{x, y},
-                        Tile::Wall => return init_pos,
-                        Tile::StepThrough => (),
-                    }
-                }
-                panic!()
-            },
-            Dir::Down => {
-                let x = pos.x;
-                let ys1 = (pos.y+1)..self.height;
-                let ys2 = 0..pos.y;
-                for y in ys1.chain(ys2) {
-                    match self[Pos{x, y}] {
-                        Tile::Open => return Pos{x, y},
-                        Tile::Wall => return init_pos,
-                        Tile::StepThrough => (),
-                    }
-                }
-                panic!()
-            },
-            Dir::Left => {
-                let y = pos.y;
-                let xs1 = (0..pos.x).rev();
-                let xs2 = (pos.x..self.width).rev();
-                for x in xs1.chain(xs2) {
-                    match self[Pos{x, y}] {
-                        Tile::Open => return Pos{x, y},
-                        Tile::Wall => return init_pos,
-                        Tile::StepThrough => (),
-                    }
-                }
-                panic!()
-            },
-            Dir::Right => {
-                let y = pos.y;
-                let xs1 = (pos.x+1)..self.width;
-                let xs2 = 0..pos.x;
-                for x in xs1.chain(xs2) {
-                    match self[Pos{x, y}] {
-                        Tile::Open => return Pos{x, y},
-                        Tile::Wall => return init_pos,
-                        Tile::StepThrough => (),
-                    }
-                }
-                panic!()
-            },
+    fn walk_one(&self, pos: Pos, dir: Dir) -> (Pos, Dir) {
+        if let Some((next_pos, next_dir)) = self.jumps.get(&(pos, dir)) {
+            match self.tiles[next_pos] {
+                Tile::Open => (*next_pos, *next_dir),
+                Tile::Wall => (pos, dir),
+            }
+        } else {
+            match self.tiles[&(pos + dir.as_offset())] {
+                Tile::Open => (pos + dir.as_offset(), dir),
+                Tile::Wall => (pos, dir),
+            }
         }
     }
 }
 
-impl Index<Pos> for Grid {
-    type Output = Tile;
-    fn index(&self, pos: Pos) -> &Tile {
-        debug_assert!(pos.x < self.width);
-        debug_assert!(pos.y < self.height);
-        &self.tiles[self.width * pos.y + pos.x]
-    }
-}
-
-fn parse_grid(input: &str) -> Grid {
-    let height = input.lines().count();
-    let width = input.lines().map(|line| line.len()).max().unwrap();
-    let mut tiles = vec![Tile::StepThrough; width * height];
+fn parse_raw_grid(input: &str, block_size: usize) -> Grid {
+    let mut initial_pos: Option<Pos> = None;
+    let mut tiles = HashMap::new();
     for (y, line) in input.lines().enumerate() {
+        let y = y as isize;
         for (x, ch) in line.chars().enumerate() {
+            let pos = Pos { x: x as isize, y };
             let tile = match ch {
-                ' ' => Tile::StepThrough,
+                ' ' => continue,
                 '#' => Tile::Wall,
                 '.' => Tile::Open,
                 _ => panic!(),
             };
-            tiles[y * width + x] = tile;
+            if tile == Tile::Open && initial_pos == None {
+                initial_pos = Some(pos);
+            }
+            tiles.insert(pos, tile);
         }
     }
-    Grid { width, height, tiles }
+    Grid {
+        initial_pos: initial_pos.unwrap(),
+        tiles,
+        block_size,
+        jumps: HashMap::new(),
+    }
 }
 
 fn parse_steps(input: &str) -> Vec<Step> {
@@ -182,33 +174,47 @@ fn parse_steps(input: &str) -> Vec<Step> {
     ret
 }
 
-fn parse(puzzle_input: &str) -> (Grid, Vec<Step>) {
+fn parse_raw(puzzle_input: &str, block_size: usize) -> (Grid, Vec<Step>) {
     let puzzle_input = puzzle_input.trim_end();
     let mut sp_it = puzzle_input.split("\n\n");
-    let grid = parse_grid(sp_it.next().unwrap());
+    let grid = parse_raw_grid(sp_it.next().unwrap(), block_size);
     let steps = parse_steps(sp_it.next().unwrap());
     assert!(sp_it.next().is_none());
     (grid, steps)
 }
 
-fn initial_tile(grid: &Grid) -> Pos {
-    for x in 0..grid.width {
-        let pos = Pos{ x, y: 0 };
-        if grid[pos] == Tile::Open {
-            return pos;
+fn fill_jumps_part1(grid: &mut Grid) {
+    let height = grid.tiles.keys().map(|pos| pos.y).max().unwrap() + 1;
+    let width = grid.tiles.keys().map(|pos| pos.x).max().unwrap() + 1;
+    for pos in grid.tiles.keys().copied() {
+        for dir in [Dir::Up, Dir::Down, Dir::Left, Dir::Right] {
+            let mut pos2 = pos + dir.as_offset();
+            if grid.tiles.contains_key(&pos2) {
+                continue; // Just a normal step
+            }
+            loop {
+                debug_assert!(pos2 != pos);
+                if grid.tiles.contains_key(&pos2) {
+                    grid.jumps.insert((pos, dir), (pos2, dir));
+                    break;
+                }
+                pos2 += dir.as_offset();
+                if pos2.x >= width { pos2.x = 0 };
+                if pos2.x < 0 { pos2.x = width - 1 };
+                if pos2.y >= height { pos2.y = 0 };
+                if pos2.y < 0 { pos2.y = height - 1 };
+            }
         }
     }
-    panic!()
 }
 
-fn part1(puzzle_input: &str) -> usize {
-    let (grid, steps) = parse(puzzle_input);
-    let mut pos = initial_tile(&grid);
+fn walk_all(grid: &Grid, steps: &[Step]) -> isize {
+    let mut pos = grid.initial_pos;
     let mut dir = Dir::Right;
-    for step in &steps {
+    for step in steps {
         match step {
             Step::Walk(num) => for _ in 0..*num {
-                pos = grid.walk_one(pos, dir);
+                (pos, dir) = grid.walk_one(pos, dir);
             },
             Step::Turn(turn) => dir = dir.turn(*turn),
         }
@@ -216,8 +222,53 @@ fn part1(puzzle_input: &str) -> usize {
     1000 * (pos.y + 1) + 4 * (pos.x + 1) + dir.score()
 }
 
-fn part2(puzzle_input: &str) -> &str {
-    "FIXME"
+fn part1(puzzle_input: &str) -> isize {
+    let (mut grid, steps) = parse_raw(puzzle_input, 50 /* only needed for part2 */);
+    fill_jumps_part1(&mut grid);
+    walk_all(&grid, &steps)
+}
+
+fn fill_jumps_part2(grid: &mut Grid, start_pos1: Pos, dir1: Dir, start_pos2: Pos, dir2: Dir) {
+    let mut pos1s = Vec::with_capacity(grid.block_size);
+    let mut pos2s = Vec::with_capacity(grid.block_size);
+    let mut pos1 = start_pos1;
+    let mut pos2 = start_pos2;
+    for _ in 0..grid.block_size {
+        pos1s.push(pos1);
+        pos1 += dir1.as_offset();
+        pos2s.push(pos2);
+        pos2 += dir2.as_offset();
+    }
+    // Set the travel dirs to the direction toward empty space
+    let mut dir1 = dir1.turn(Turn::Right);
+    if grid.tiles.contains_key(&(start_pos1 + dir1.as_offset())) {
+        dir1 = dir1.flip();
+        assert!(!grid.tiles.contains_key(&(start_pos1 + dir1.as_offset())));
+    }
+    let mut dir2 = dir2.turn(Turn::Right);
+    if grid.tiles.contains_key(&(start_pos2 + dir2.as_offset())) {
+        dir2 = dir2.flip();
+        assert!(!grid.tiles.contains_key(&(start_pos2 + dir2.as_offset())));
+    }
+
+    for i in 0..grid.block_size {
+        grid.jumps.insert((pos1s[i], dir1), (pos2s[i], dir2.flip()));
+        grid.jumps.insert((pos2s[i], dir2), (pos1s[i], dir1.flip()));
+    }
+}
+
+fn part2(puzzle_input: &str) -> isize {
+    let (mut grid, steps) = parse_raw(puzzle_input, 50);
+    // FIXME - OK this is super hacky and only works for MY input. But I'm too lazy to map the
+    // input onto a cube programmatically
+    fill_jumps_part2(&mut grid, Pos { x: 50, y: 0 }, Dir::Right, Pos { x: 0, y: 150 }, Dir::Down);
+    fill_jumps_part2(&mut grid, Pos { x: 100, y: 0 }, Dir::Right, Pos { x: 0, y: 199 }, Dir::Right);
+    fill_jumps_part2(&mut grid, Pos { x: 50, y: 0 }, Dir::Down, Pos { x: 0, y: 149 }, Dir::Up);
+    fill_jumps_part2(&mut grid, Pos { x: 149, y: 0 }, Dir::Down, Pos { x: 99, y: 149 }, Dir::Up);
+    fill_jumps_part2(&mut grid, Pos { x: 100, y: 49 }, Dir::Right, Pos { x: 99, y: 50 }, Dir::Down);
+    fill_jumps_part2(&mut grid, Pos { x: 50, y: 50 }, Dir::Down, Pos { x: 0, y: 100 }, Dir::Right);
+    fill_jumps_part2(&mut grid, Pos { x: 50, y: 149 }, Dir::Right, Pos { x: 49, y: 150 }, Dir::Down);
+    walk_all(&grid, &steps)
 }
 
 fn main() {
@@ -255,6 +306,14 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        // FIXME
+        let (mut grid, steps) = parse_raw(EX, 4);
+        fill_jumps_part2(&mut grid, Pos { x: 8, y: 0 }, Dir::Right, Pos { x: 3, y: 4 }, Dir::Left);
+        fill_jumps_part2(&mut grid, Pos { x: 8, y: 0 }, Dir::Down, Pos { x: 4, y: 4 }, Dir::Right);
+        fill_jumps_part2(&mut grid, Pos { x: 11, y: 0 }, Dir::Down, Pos { x: 15, y: 11 }, Dir::Up);
+        fill_jumps_part2(&mut grid, Pos { x: 0, y: 4 }, Dir::Down, Pos { x: 15, y: 11 }, Dir::Left);
+        fill_jumps_part2(&mut grid, Pos { x: 11, y: 4 }, Dir::Down, Pos { x: 15, y: 8 }, Dir::Left);
+        fill_jumps_part2(&mut grid, Pos { x: 0, y: 7 }, Dir::Right, Pos { x: 11, y: 11 }, Dir::Left);
+        fill_jumps_part2(&mut grid, Pos { x: 4, y: 7 }, Dir::Right, Pos { x: 8, y: 11 }, Dir::Up);
+        assert_eq!(walk_all(&grid, &steps), 5031);
     }
 }
