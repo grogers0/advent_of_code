2fd61ea00fd5aa197ac93d321ba2be8ec26ec294
Greg Rogers <greg.r.rogers@gmail.com>
2023-12-18 14:29:11 -0500

    Day 14 2023 Part 2

--- 2023/day14/src/main.rs
+++ 2023/day14/src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::io::{self, Read};
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 enum Tile {
     RoundRock,
     CubeRock,
@@ -67,6 +68,70 @@ fn shift_rocks_north(map: &mut Map) {
     }
 }
 
+fn shift_rocks_south(map: &mut Map) {
+    for y in (0..map.height).rev() {
+        for x in 0..map.width {
+            if let Tile::RoundRock = map.at(x, y) {
+                let mut y2 = y;
+                while y2 < map.height - 1 {
+                    if let Tile::Empty = map.at(x, y2+1) {
+                        map.set(x, y2, Tile::Empty);
+                        map.set(x, y2+1, Tile::RoundRock);
+                        y2 += 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn shift_rocks_west(map: &mut Map) {
+    for y in 0..map.height {
+        for x in 0..map.width {
+            if let Tile::RoundRock = map.at(x, y) {
+                let mut x2 = x;
+                while x2 > 0 {
+                    if let Tile::Empty = map.at(x2-1, y) {
+                        map.set(x2, y, Tile::Empty);
+                        map.set(x2-1, y, Tile::RoundRock);
+                        x2 -= 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn shift_rocks_east(map: &mut Map) {
+    for y in 0..map.height {
+        for x in (0..map.width).rev() {
+            if let Tile::RoundRock = map.at(x, y) {
+                let mut x2 = x;
+                while x2 < map.width - 1 {
+                    if let Tile::Empty = map.at(x2+1, y) {
+                        map.set(x2, y, Tile::Empty);
+                        map.set(x2+1, y, Tile::RoundRock);
+                        x2 += 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn shift_rocks_cycle(map: &mut Map) {
+    shift_rocks_north(map);
+    shift_rocks_west(map);
+    shift_rocks_south(map);
+    shift_rocks_east(map);
+}
+
 #[allow(dead_code)]
 fn print_map(map: &Map) -> String {
     let mut s = String::new();
@@ -101,8 +166,19 @@ fn part1(mut map: Map) -> usize {
 }
 
 fn part2(mut map: Map) -> usize {
-    // FIXME
-    calc_load(&map)
+    let mut saved = HashMap::new();
+    const N_ITERS: u32 = 1_000_000_000;
+    for i in 0..N_ITERS {
+        if let Some(prior_iter) = saved.insert(map.tiles.clone(), i) {
+            let j = i + ((N_ITERS - i) / (i - prior_iter) * (i - prior_iter));
+            for _ in j..N_ITERS {
+                shift_rocks_cycle(&mut map);
+            }
+            return calc_load(&map);
+        }
+        shift_rocks_cycle(&mut map);
+    }
+    panic!()
 }
 
 fn main() {
