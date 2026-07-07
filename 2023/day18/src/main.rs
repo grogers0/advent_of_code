b0f5473b914c51e88a665237a42fb5e651ba353f
Greg Rogers <greg.r.rogers@gmail.com>
2026-07-07 15:12:39 -0400

    Day 18 2023 part 2

--- 2023/day18/src/main.rs
+++ 2023/day18/src/main.rs
@@ -11,14 +11,11 @@ enum Dir {
 struct Instruction {
     dir: Dir,
     dist: usize,
-    // color: u32,
 }
 
 struct Map {
     width: usize,
     height: usize,
-    start_x: usize,
-    start_y: usize,
     tiles: Vec<bool>,
 }
 
@@ -38,55 +35,6 @@ impl Map {
     }
 }
 
-fn parse(puzzle_input: &str) -> Vec<Instruction> {
-    puzzle_input.lines().map(|line| {
-        let mut sp_iter = line.split(" ");
-        let dir = match sp_iter.next().unwrap() {
-            "R" => Dir::Right,
-            "L" => Dir::Left,
-            "D" => Dir::Down,
-            "U" => Dir::Up,
-            _ => panic!(),
-        };
-        let dist = sp_iter.next().unwrap().parse().unwrap();
-        Instruction { dir, dist }
-    }).collect()
-}
-
-// NOTE - the map is not filled
-fn discover_bounds(instructions: &[Instruction]) -> Map {
-    let mut x = 0isize;
-    let mut y = 0isize;
-    let mut min_x = 0;
-    let mut max_x = 0;
-    let mut min_y = 0;
-    let mut max_y = 0;
-    for inst in instructions {
-        for _ in 0..inst.dist {
-            match inst.dir {
-                Dir::Up => y -= 1,
-                Dir::Down => y += 1,
-                Dir::Left => x -= 1,
-                Dir::Right => x += 1,
-            }
-        }
-        min_x = min(min_x, x);
-        max_x = max(max_x, x);
-        min_y = min(min_y, y);
-        max_y = max(max_y, y);
-    }
-    let width = (max_x - min_x + 1) as usize;
-    let height = (max_y - min_y + 1) as usize;
-    let tiles = vec![false; width * height];
-    Map {
-        width,
-        height,
-        start_x: (-min_x) as usize,
-        start_y: (-min_y) as usize,
-        tiles,
-    }
-}
-
 impl fmt::Display for Map {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for y in 0..self.height {
@@ -103,22 +51,151 @@ impl fmt::Display for Map {
     }
 }
 
-fn part1(instructions: &[Instruction]) -> usize {
-    let mut map = discover_bounds(instructions);
-    let mut x = map.start_x;
-    let mut y = map.start_y;
+fn parse(puzzle_input: &str) -> (Vec<Instruction>, Vec<Instruction>) {
+    let mut instructions1 = Vec::new();
+    let mut instructions2 = Vec::new();
+    for line in puzzle_input.lines() {
+        let mut sp = line.split(" ");
+        {
+            let dir = match sp.next().unwrap() {
+                "R" => Dir::Right,
+                "L" => Dir::Left,
+                "D" => Dir::Down,
+                "U" => Dir::Up,
+                _ => panic!(),
+            };
+            let dist = sp.next().unwrap().parse().unwrap();
+            instructions1.push(Instruction { dir, dist });
+        }
+
+        {
+            let color = sp.next().unwrap();
+            assert!(sp.next().is_none());
+            assert!(color.starts_with("(#"));
+            assert!(color.ends_with(")"));
+            let dir = match &color[(color.len() - 2)..(color.len() - 1)] {
+                "0" => Dir::Right,
+                "1" => Dir::Down,
+                "2" => Dir::Left,
+                "3" => Dir::Up,
+                _ => panic!(),
+            };
+            let dist = usize::from_str_radix(&color[2..(color.len() - 2)], 16).unwrap();
+            instructions2.push(Instruction { dir, dist });
+        }
+    }
+    (instructions1, instructions2)
+}
+
+// The instructions may start anywhere on the map, find the bounds of the map and then shift the
+// x/y start to the appropriate position to make indexes only in 0..N
+// Returns (width, height, xstart, ystart)
+fn discover_bounds(instructions: &[Instruction]) -> (usize, usize, usize, usize) {
+    let mut x = 0isize;
+    let mut y = 0isize;
+    let mut xmin = 0;
+    let mut xmax = 0;
+    let mut ymin = 0;
+    let mut ymax = 0;
+    for inst in instructions {
+        match inst.dir {
+            Dir::Up => y -= inst.dist as isize,
+            Dir::Down => y += inst.dist as isize,
+            Dir::Left => x -= inst.dist as isize,
+            Dir::Right => x += inst.dist as isize,
+        }
+        xmin = min(xmin, x);
+        xmax = max(xmax, x);
+        ymin = min(ymin, y);
+        ymax = max(ymax, y);
+    }
+    let width = (xmax - xmin + 1) as usize;
+    let height = (ymax - ymin + 1) as usize;
+    let xstart = (-xmin) as usize;
+    let ystart = (-ymin) as usize;
+    (width, height, xstart, ystart)
+}
+
+// Calculate the change of basis for x/y to compress the coordinates
+fn change_basis(instructions: &[Instruction], xstart: usize, ystart: usize) -> (Vec<usize>, Vec<usize>) {
+    let mut xs = Vec::new();
+    let mut ys = Vec::new();
+    let mut x = xstart;
+    let mut y = ystart;
+
+    xs.push(x);
+    ys.push(y);
+    for inst in instructions {
+        match inst.dir {
+            Dir::Up => y -= inst.dist,
+            Dir::Down => y += inst.dist,
+            Dir::Left => x -= inst.dist,
+            Dir::Right => x += inst.dist,
+        }
+        xs.push(x);
+        ys.push(y);
+    }
+    xs.sort();
+    ys.sort();
+    xs.dedup();
+    ys.dedup();
+
+    let compress_basis = |vals: &[usize]| -> Vec<usize> {
+        let mut counts = Vec::new();
+        let mut prev = 0;
+        for &curr in vals {
+            if curr - prev > 1 {
+                counts.push(curr - prev - 1);
+            }
+            counts.push(1);
+            prev = curr;
+        }
+        counts
+    };
+    let xcounts = compress_basis(&xs);
+    let ycounts = compress_basis(&ys);
+    (xcounts, ycounts)
+}
+
+fn build_map(instructions: &[Instruction], xstart: usize, ystart: usize, xcounts: &[usize], ycounts: &[usize]) -> Map {
+    let find_start = |start: usize, counts: &[usize]| -> usize {
+        let mut sum = 0;
+        for i in 0..counts.len() {
+            assert!(start >= sum);
+            if start == sum { return i; }
+            sum += counts[i];
+        }
+        sum
+    };
+    let mut map = Map {
+        width: xcounts.len(),
+        height: ycounts.len(),
+        tiles: vec![false; xcounts.len() * ycounts.len()],
+    };
+    let mut x = find_start(xstart, xcounts);
+    let mut y = find_start(ystart, ycounts);
     map.fill(x, y);
     for inst in instructions {
-        for _ in 0..inst.dist {
+        let mut dist = 0;
+        while dist < inst.dist {
             match inst.dir {
-                Dir::Up => y -= 1,
-                Dir::Down => y += 1,
-                Dir::Left => x -= 1,
-                Dir::Right => x += 1,
+                Dir::Up => { dist += ycounts[y]; y -= 1; },
+                Dir::Down => { dist += ycounts[y]; y += 1; },
+                Dir::Left => { dist += xcounts[x]; x -= 1; },
+                Dir::Right => { dist += xcounts[x]; x += 1; },
             }
             map.fill(x, y);
         }
+        assert_eq!(dist, inst.dist);
     }
+
+    map
+}
+
+fn calc(instructions: &[Instruction]) -> u64 {
+    let (_width, _height, xstart, ystart) = discover_bounds(instructions);
+    let (xcounts, ycounts) = change_basis(instructions, xstart, ystart);
+    let map = build_map(instructions, xstart, ystart, &xcounts, &ycounts);
 
     let mut outside = HashSet::new();
     let mut pending = Vec::new();
@@ -141,20 +218,21 @@ fn part1(instructions: &[Instruction]) -> usize {
         if y < map.height-1 { pending.push((x, y+1)); }
     }
 
-    (map.width * map.height) - outside.len()
-}
-
-fn part2(puzzle_input: &str) -> &str {
-    "FIXME"
+    let mut ret = xcounts.iter().cloned().sum::<usize>() as u64 *
+        ycounts.iter().cloned().sum::<usize>() as u64;
+    for (x, y) in outside {
+        ret -= xcounts[x] as u64 * ycounts[y] as u64;
+    }
+    ret
 }
 
 fn main() {
     let mut puzzle_input = String::new();
     io::stdin().read_to_string(&mut puzzle_input).unwrap();
 
-    let instructions = parse(&puzzle_input);
-    println!("{}", part1(&instructions));
-    println!("{}", part2(&puzzle_input));
+    let (instructions1, instructions2) = parse(&puzzle_input);
+    println!("{}", calc(&instructions1));
+    println!("{}", calc(&instructions2));
 }
 
 #[cfg(test)]
@@ -178,11 +256,11 @@ U 2 (#7a21e3)";
     
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&parse(EX)), 62);
+        assert_eq!(calc(&parse(EX).0), 62);
     }
 
     #[test]
     fn test_part2() {
-        // FIXME
+        assert_eq!(calc(&parse(EX).1), 952408144115);
     }
 }
