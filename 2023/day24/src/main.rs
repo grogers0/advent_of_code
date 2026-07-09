28ae163fc9875642e2cf925ae398b0dbd17f89fc
Greg Rogers <greg.r.rogers@gmail.com>
2026-07-09 11:01:06 -0400

    Day 24 2023 part 2

--- 2023/day24/src/main.rs
+++ 2023/day24/src/main.rs
@@ -2,20 +2,70 @@ use std::io::{self, Read};
 
 use regex::Regex;
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 struct Pos {
-    x: i64,
-    y: i64,
-    z: i64,
+    x: i128,
+    y: i128,
+    z: i128,
 }
 
 impl Pos {
-    fn new(x: i64, y: i64, z: i64) -> Pos {
+    fn new(x: i128, y: i128, z: i128) -> Pos {
         Pos { x, y, z }
+    }
+
+    fn cross(self, other: Pos) -> Pos {
+        Pos {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    fn dot(self, other: Pos) -> i128 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    fn mul(self, factor: i128) -> Pos {
+        Pos {
+            x: self.x * factor,
+            y: self.y * factor,
+            z: self.z * factor,
+        }
+    }
+
+    fn div(self, factor: i128) -> Pos {
+        Pos {
+            x: self.x / factor,
+            y: self.y / factor,
+            z: self.z / factor,
+        }
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+impl std::ops::Sub for Pos {
+    type Output = Pos;
+    fn sub(self, other: Pos) -> Pos {
+        Pos {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl std::ops::Add for Pos {
+    type Output = Pos;
+    fn add(self, other: Pos) -> Pos {
+        Pos {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 struct Ray {
     pos: Pos,
     dir: Pos,
@@ -36,26 +86,29 @@ fn parse(puzzle_input: &str) -> Vec<Ray> {
 }
 
 
-fn intersect2d(a: &Ray, b: &Ray, min_xy: i64, max_xy: i64) -> bool {
+fn intersect2d(a: Ray, b: Ray, min_xy: i128, max_xy: i128) -> bool {
     let dx = b.pos.x - a.pos.x;
     let dy = b.pos.y - a.pos.y;
-    let det = b.dir.x * a.dir.y - b.dir.y * a.dir.x;
+    let mut det = b.dir.x * a.dir.y - b.dir.y * a.dir.x;
     if det == 0 { return false }
-    // FIXME - this doesn't handle non-integer overlapping coordinates, which makes it fail the
-    // test input, but it works well enough to pass the actual input
-    let u = (dy * b.dir.x - dx * b.dir.y) / det;
-    let v = (dy * a.dir.x - dx * a.dir.y) / det;
-    if u < 0 || v < 0 { return false }
-    let ix = a.pos.x + a.dir.x * u;
-    let iy = a.pos.y + a.dir.y * u;
+    let mut u_num = dy * b.dir.x - dx * b.dir.y;
+    let mut v_num = dy * a.dir.x - dx * a.dir.y;
+    if det < 0 {
+        det = -det;
+        u_num = -u_num;
+        v_num = -v_num;
+    }
+    if u_num < 0 || v_num < 0 { return false }
+    let ix = (a.pos.x * det + a.dir.x * u_num) / det;
+    let iy = (a.pos.y * det + a.dir.y * u_num) / det;
     ix >= min_xy && ix <= max_xy && iy >= min_xy && iy <= max_xy
 }
 
-fn count_intersections2d(rays: &[Ray], min_xy: i64, max_xy: i64) -> usize {
+fn part1(rays: &[Ray], min_xy: i128, max_xy: i128) -> usize {
     let mut cnt = 0;
     for i in 0..(rays.len()-1) {
         for j in (i+1)..rays.len() {
-            if intersect2d(&rays[i], &rays[j], min_xy, max_xy) {
+            if intersect2d(rays[i], rays[j], min_xy, max_xy) {
                 cnt += 1;
             }
         }
@@ -63,12 +116,23 @@ fn count_intersections2d(rays: &[Ray], min_xy: i64, max_xy: i64) -> usize {
     cnt
 }
 
-fn part1(rays: &[Ray]) -> usize {
-    count_intersections2d(rays, 200000000000000, 400000000000000)
-}
-
-fn part2(puzzle_input: &str) -> &str {
-    "FIXME"
+// I didn't feel like trying to understand the math of intersections in 3d so I just followed:
+// https://www.reddit.com/r/adventofcode/comments/18pnycy/comment/kxqjg33/
+fn part2(rays: &[Ray]) -> i128 {
+    let ray0 = rays[0];
+    let ray1 = rays[1];
+    let ray2 = rays[2];
+    let p1 = ray1.pos - ray0.pos;
+    let v1 = ray1.dir - ray0.dir;
+    let p2 = ray2.pos - ray0.pos;
+    let v2 = ray2.dir - ray0.dir;
+    let t1 = -(p1.cross(p2).dot(v2) / v1.cross(p2).dot(v2));
+    let t2 = -(p1.cross(p2).dot(v1) / p1.cross(v2).dot(v1));
+    let c1 = ray1.pos + ray1.dir.mul(t1);
+    let c2 = ray2.pos + ray2.dir.mul(t2);
+    let v = (c2 - c1).div(t2 - t1);
+    let p = c1 - v.mul(t1);
+    p.x + p.y + p.z
 }
 
 fn main() {
@@ -76,8 +140,8 @@ fn main() {
     io::stdin().read_to_string(&mut puzzle_input).unwrap();
 
     let rays = parse(&puzzle_input);
-    println!("{}", part1(&rays));
-    println!("{}", part2(&puzzle_input));
+    println!("{}", part1(&rays, 200000000000000, 400000000000000));
+    println!("{}", part2(&rays));
 }
 
 #[cfg(test)]
@@ -92,11 +156,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(count_intersections2d(&parse(EX), 7, 27), 2);
+        assert_eq!(part1(&parse(EX), 7, 27), 2);
     }
 
     #[test]
     fn test_part2() {
-        // FIXME
+        assert_eq!(part2(&parse(EX)), 47);
     }
 }
